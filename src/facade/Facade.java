e44c3c417d5c23ac4d694fe1c1a65003592df94e
Lucas Miranda <lucasmirandadourado@gmail.com>
2015-03-22 16:30:37 -0300

    Padrão FACADE
    Tentando melhorar os problemas apontados por Thiago: Colocando o padrão
    Facade

--- src/facade/Facade.java
+++ src/facade/Facade.java
@@ -3,11 +3,30 @@
  */
 package facade;
 
+import com.model.Usuario;
+
 /**
  * @author Lucas Miranda
  *
  */
 public class Facade {
 
+	String login, senha, nome, endereco, email;
+	UsuarioEasyFacade usuario; 
 	
+	public Facade(String login, String senha, String nome, String endereco, String email) { 
+		this.login = login;
+		this.senha = senha; 
+		this.nome =nome; 
+		this.endereco = endereco; 
+		this.email = email;
+		
+		this.usuario = new UsuarioEasyFacade();
+		
+		usuario.criarUsuario(login, senha, nome, endereco, email);
+	
+		this.usuario.abrirSessao(login, senha);
+	
+		this.usuario.encerrarSistema();
+	}
 }
