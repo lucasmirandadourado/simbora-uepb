9af4e80597d80f826b3fb40d1a5d355c28c38ffc
brunojclementino <brunojclementino@gmail.com>
2015-03-24 18:19:10 -0300

    Facade
    :+1:

--- src/easyaccept/SimboraEasyAccept.java
+++ src/easyaccept/SimboraEasyAccept.java
@@ -1,11 +1,11 @@
-package facade;
+package easyaccept;
 
 import com.excessoes.UsuarioException;
 
 import controller.UsuarioController;
 
-public class UsuarioEasyFacade {
-	
+public class SimboraEasyAccept {
+
 	UsuarioController usuarioController = new UsuarioController();
 	
 	public void zerarSistema(){
@@ -13,24 +13,18 @@ public class UsuarioEasyFacade {
 	}
 
 	public void criarUsuario(String login, String senha, String nome, String endereco, String email) throws UsuarioException{
-
 		usuarioController.criarUsuario(login, senha, nome, endereco, email);
-		
 	}
 	
 	public int abrirSessao(String login, String senha) throws UsuarioException{
-
 		return usuarioController.abrirSessao(login, senha);
 	}
 
 	public String getAtributoUsuario(String login, String atributo) throws UsuarioException{
-		
 		return usuarioController.getAtributoUsuario(login, atributo);
-		
 	}
 	
 	public void encerrarSistema(){
 		usuarioController.encerrarSistema();
 	}
-	
 }
---
+++ src/facade/SimboraFacade.java
@@ -0,0 +1,37 @@
+package facade;
+
+import com.excessoes.UsuarioException;
+
+import easyaccept.EasyAccept;
+import easyaccept.SimboraEasyAccept;
+
+public class SimboraFacade {
+	
+	SimboraEasyAccept simboraEasyAccept = new SimboraEasyAccept();
+	
+	public void zerarSistema(){
+		simboraEasyAccept.zerarSistema();
+	}
+
+	public void criarUsuario(String login, String senha, String nome, String endereco, String email) throws UsuarioException{
+		simboraEasyAccept.criarUsuario(login, senha, nome, endereco, email);
+	}
+	
+	public int abrirSessao(String login, String senha) throws UsuarioException{
+		return simboraEasyAccept.abrirSessao(login, senha);
+	}
+
+	public String getAtributoUsuario(String login, String atributo) throws UsuarioException{
+		return simboraEasyAccept.getAtributoUsuario(login, atributo);
+	}
+	
+	public void encerrarSistema(){
+		simboraEasyAccept.encerrarSistema();
+	}
+	
+
+	public static void main(String[] args) {
+		args = new String[] {"facade.SimboraFacade", "scripts/us01.txt", "scripts/us02.txt"};
+		EasyAccept.main(args); 
+	}
+}
