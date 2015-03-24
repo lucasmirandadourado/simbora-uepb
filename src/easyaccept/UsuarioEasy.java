511348d06a85347ae1589be07a63df91359c2609
Lucas Miranda <lucasmirandadourado@gmail.com>
2015-03-24 17:11:41 -0300

    Nomes das classes
    us3

--- src/easyaccept/UsuarioEasy.java
+++ src/easyaccept/UsuarioEasy.java
@@ -3,7 +3,7 @@ package easyaccept;
 public class UsuarioEasy {
 
 	public static void main(String[] args) {
-		args = new String[] {"facade.UsuarioEasyFacade", "scripts/us01.txt"};
+		args = new String[] {"facade.UsuarioEasyFacade", "scripts/us03.txt"};
 		EasyAccept.main(args); 
 	}
 }
