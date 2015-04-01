a2610b2aaba2785fdec63efaff33cdc828ede286
brunojclementino <brunojclementino@gmail.com>
2015-04-01 11:36:46 -0300

    :'(
    Exclusão das classes consideradas desnecessárias

---
+++ src/com/model/Sessao.java
@@ -0,0 +1,37 @@
+package com.model;
+
+public class Sessao {
+	
+	private String idSessao;
+	private String idUsuario;
+	
+	private boolean statusSessao = false;
+
+	public String getIdSessao() {
+		return idSessao;
+	}
+
+	public void setIdSessao(String idSessao) {
+		this.idSessao = idSessao;
+	}
+
+	public String getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public boolean isStatusSessao() {
+		return statusSessao;
+	}
+
+	public void setStatusSessao(boolean statusSessao) {
+		this.statusSessao = statusSessao;
+	}
+	
+	
+	
+
+}
---
+++ src/controller/SessaoController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.model.Sessao;
+
+public class SessaoController {
+	
+	Sessao sessao;
+	List<Sessao> sessoes = new ArrayList<>();
+	
+	
+
+
+}
