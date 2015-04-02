6a72982557b27d4642be55710c5b3c26885d9fb9
Lucas Miranda Dourado <lucasmirandadourado@gmail.com>
2015-04-02 11:36:08 -0300

    up
    up

--- src/com/controller/SessaoController.java
+++ src/com/controller/SessaoController.java
@@ -3,14 +3,29 @@ package com.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.excessoes.UsuarioException;
 import com.model.Sessao;
+import com.model.Usuario;
 
 public class SessaoController {
 	
-	Sessao sessao;
 	List<Sessao> sessoes = new ArrayList<>();
+	List<Usuario> usuarios = new ArrayList<>();
 	
+	public String abrirSessao(String login, String senha) throws UsuarioException{
 	
+		for(Usuario usuario : usuarios){
+				if( usuario.getLogin().equals(login) && usuario.getSenha().equals(senha)){
+					return usuarios.indexOf(usuario)+"";	
+				}
+				else if(login==null || login.isEmpty() || usuario.getLogin().equals(login) || usuario.getSenha().equals(senha)){
+					//mensagemErro = "Login inválido";
+					throw new UsuarioException("Login inválido");
+				}
+		}
+	
+		throw new UsuarioException("Usuário inexistente");
+	}
 
 
 }
