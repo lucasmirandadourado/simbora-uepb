7cd43707af113e8bc9ac9d5f49ff4b28f47419fd
Lucas Miranda Dourado <lucasmirandadourado@gmail.com>
2015-04-04 16:11:39 -0300

    SolicitarCaronaController
    up

--- src/com/controller/CaronaController.java
+++ src/com/controller/CaronaController.java
@@ -280,25 +280,5 @@ public class CaronaController {
 				+ ", as " + carona.getHorarioDeSaida();
 	}
 
-	// US04
-	/**
-	 * Quando o caroneiro aceitar, deve-se atualizar a quantidade de vagas do carro.
-	 * @param idSessao
-	 * @param idSolicitacao
-	 */
-	public void aceitarSolicitacaoPontoEncontro(String idSessao, String idSolicitacao) {
-		
-		
-	}
-	
-	public String sugerirPontoEncontro(String idSessao, String idCarona, String pontos) {
-		return "";
-	}
-	
-	public String responderSugestaoPontoEncontro(String idSessao,
-			String idCarona, String idSugestao, String pontos) {
-
-		return "";
-	}
 
 }
--- src/com/controller/SolicitacaoVagaController.java
+++ src/com/controller/SolicitacaoVagaController.java
@@ -4,9 +4,31 @@
 package com.controller;
 
 /**
+ * 
  * @author Lucas Miranda e Bruno Clementino
  *
  */
 public class SolicitacaoVagaController {
+
+	// US04
+	/**
+	 * Quando o caroneiro aceitar, deve-se atualizar a quantidade de vagas do carro.
+	 * @param idSessao
+	 * @param idSolicitacao
+	 */
+	public void aceitarSolicitacaoPontoEncontro(String idSessao, String idSolicitacao) { 
+		
+		
+	}
+	
+	public String sugerirPontoEncontro(String idSessao, String idCarona, String pontos) {
+		return "";
+	}
+	
+	public String responderSugestaoPontoEncontro(String idSessao,
+			String idCarona, String idSugestao, String pontos) {
+
+		return "";
+	}
 
 }
