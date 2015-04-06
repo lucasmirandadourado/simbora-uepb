1e4ccee8a30a910cad7da47dfb84d7bae9e129e2
Lucas Miranda Dourado <lucasmirandadourado@gmail.com>
2015-04-06 13:20:15 -0300

    Teste
    SolicitaçaoCarona

--- src/com/controller/SessaoController.java
+++ src/com/controller/SessaoController.java
@@ -22,35 +22,49 @@ public class SessaoController {
 	 */
 	private static List<Usuario> usuarios = new ArrayList<>();
 
-	public String abrirSessao(String login, String senha) throws SessaoException {
+	/**
+	 * Ao abrir a sessão é necessário o login e senha do usuário. Esse metodo
+	 * retornará o idSessao que é o login do usuário. Caso os parametros tenham sido 
+	 * inseridos errados será gerado um erro de <b>Usuário inexistente</b>
+	 *
+	 *<br>@see {@link Usuario} 
+	 * @param login
+	 * @param senha
+	 * @return
+	 * @throws SessaoException
+	 *
+	 */
+	public String abrirSessao(String login, String senha)
+			throws SessaoException {
 		for (Usuario usuario : usuarios) {
-			if (usuario.getLogin().equals(login) && usuario.getSenha().equals(senha)) {
-					Sessao ss = new Sessao();
-						ss.setIdSessao(usuario.getLogin());
-						ss.setIdUsuario(login);
-						sessoes.add(ss);
+			if (usuario.getLogin().equals(login)
+					&& usuario.getSenha().equals(senha)) {
+				Sessao ss = new Sessao();
+				ss.setIdSessao(usuario.getLogin());
+				ss.setIdUsuario(login);
+				sessoes.add(ss);
 				return ss.getIdSessao();
 			} else if (login == null || login.isEmpty()
 					|| usuario.getLogin().equals(login)
 					|| usuario.getSenha().equals(senha)) {
-				// mensagemErro = "Login inválido";
 				throw new SessaoException("Login inválido");
 			}
 		}
 		throw new SessaoException("Usuário inexistente");
 	}
 
-	
 	public void encerrarSessao(String login) {
 		for (Usuario usuario : usuarios) {
 			if (usuario.getLogin().equals(login)) {
-				sessoes.remove(usuario);  // Não deveria retirar o usuário da sessao?
-				//usuarios.remove(usuario); // Remover o usuário seria tirar ele do cadastro não seria???
+				sessoes.remove(usuario); // Não deveria retirar o usuário da
+											// sessao?
+				// usuarios.remove(usuario); // Remover o usuário seria tirar
+				// ele do cadastro não seria???
 				break;
 			}
 		}
 	}
-	
+
 	public static boolean hasSessao(String idSessao) {
 		for (Sessao sessao : sessoes) {
 			if (sessao.getIdSessao().equals(idSessao)) {
@@ -59,7 +73,7 @@ public class SessaoController {
 		}
 		return false;
 	}
-	
+
 	public static List<Sessao> getSessoes() {
 		return sessoes;
 	}
